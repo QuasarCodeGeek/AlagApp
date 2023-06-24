93b670b14a4729b6992e2c2b53866b7d870a1540
Jayce Seyer <jaysonreyes.jsr11@gmail.com>
2023-06-24 22:32:14 +0800

    Update connector.php

--- admin/api/connector.php
+++ admin/api/connector.php
@@ -6,7 +6,6 @@ $password = "root";
     try{
         $connect = new PDO("mysql:host = $servername; dbname = alagapp_db", $username, $password);
         $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected to Database";
     } catch(PDOException $e){
         echo "Connection Failed: ".$e->getMessage();        
     }
